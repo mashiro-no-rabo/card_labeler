d11acf4e696b11aeaabb5e45a17c31cd66163f2d
AquarHEAD Lou <aquarhead@ela.build>
2016-11-12 06:12:52 +0000

    Add supervision tree to start workers from config

--- lib/card_labeler.ex
+++ lib/card_labeler.ex
@@ -5,16 +5,11 @@ defmodule CardLabeler do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(CardLabeler.Worker, [], restart: :transient)
+      supervisor(CardLabeler.WorkerSupervisor, []),
+      worker(CardLabeler.WorkerConfiger, [])
     ]
 
-    opts = [strategy: :simple_one_for_one, name: CardLabeler.Supervisor]
+    opts = [strategy: :rest_for_one, name: CardLabeler.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  def work() do
-    Enum.each(Application.get_env(:card_labeler, CardLabeler)[:worker_configs], fn config ->
-      Supervisor.start_child(CardLabeler.Supervisor, [config])
-    end)
   end
 end
