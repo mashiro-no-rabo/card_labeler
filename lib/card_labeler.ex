f32a9f30e279c9cddd04cd9a81c332e877a84776
AquarHEAD Lou <aquarhead@ela.build>
2016-11-12 12:03:42 +0000

    Actually I don't need a simple_one_for_one supervisor

--- lib/card_labeler.ex
+++ lib/card_labeler.ex
@@ -4,12 +4,12 @@ defmodule CardLabeler do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      supervisor(CardLabeler.WorkerSupervisor, []),
-      worker(CardLabeler.WorkerConfiger, [])
-    ]
+    children =
+      Enum.map(Application.get_env(:card_labeler, CardLabeler)[:worker_configs], fn config ->
+        worker(CardLabeler.Worker, [config], restart: :transient)
+      end)
 
-    opts = [strategy: :rest_for_one, name: CardLabeler.Supervisor]
+    opts = [strategy: :one_for_one, name: CardLabeler.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
