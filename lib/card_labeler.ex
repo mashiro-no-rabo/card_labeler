2b671c8e4b228a9be476b903cffd3a00f932fba5
AquarHEAD Lou <aquarhead@ela.build>
2016-11-12 03:08:25 +0000

    Update root supervisor

--- lib/card_labeler.ex
+++ lib/card_labeler.ex
@@ -5,9 +5,10 @@ defmodule CardLabeler do
     import Supervisor.Spec, warn: false
 
     children = [
+      worker(CardLabeler.Worker, [], restart: :transient)
     ]
 
-    opts = [strategy: :one_for_one, name: CardLabeler.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: CardLabeler.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
